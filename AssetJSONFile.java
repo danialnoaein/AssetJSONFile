430a93bb6510ec21760e28aec1484158d164485d
Danial Noaein <rbkk.noaein@gmail.com>
2017-08-05 22:30:13 +0430

    Create AssetJSONFile.java
    a code for reading json files from assets folder in android.

---
+++ AssetJSONFile.java
@@ -0,0 +1,8 @@
+public static String AssetJSONFile(String filename, Context context) throws IOException {
+        AssetManager manager = context.getAssets();
+        InputStream file = manager.open(filename);
+        byte[] formArray = new byte[file.available()];
+        file.read(formArray);
+        file.close();
+        return new String(formArray);
+    }
